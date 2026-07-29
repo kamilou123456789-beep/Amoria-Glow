aea96368a54fcac496a02b49c1c9c95454ca96ff
kamilou123456789-beep <kamilou123456789@gmail.com>
2026-07-29 20:26:43 +0200

    Update get-avis.js

--- netlify/functions/get-avis.js
+++ netlify/functions/get-avis.js
@@ -1,1 +1,18 @@
+exports.handler = async function(event, context) {
+  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby61xxibBlw_VvalSENF96qiCjoda9nOK4ZczqluMcOKsaJq-1EsGa_UIMSzWUZYeSn/exec';
 
+  try {
+    const response = await fetch(SCRIPT_URL);
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};S
