8e312ae256abed233e7bdf8775999f0d436c81af
kamilou123456789-beep <kamilou123456789@gmail.com>
2026-07-29 19:02:43 +0200

    Update get-stock.js

--- netlify/functions/get-stock.js
+++ netlify/functions/get-stock.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event, context) {
   const SHEETS_CSV = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQTkUQ3UTjLhTTVv29vl_dkOtjweQ41gkUJNgwDkxgb_6Y0OtGX-8FsP7ZeQzNmEykeKifIXI5o42_4/pub?gid=1075508092&single=true&output=csv';
 
