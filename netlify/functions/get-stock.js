eabe36a629ebe5f083471fb616226a0b63c15d99
kamilou123456789-beep <kamilou123456789@gmail.com>
2026-07-29 21:21:32 +0200

    Update get-stock.js

--- netlify/functions/get-stock.js
+++ netlify/functions/get-stock.js
@@ -1,19 +1,17 @@
 exports.handler = async function(event, context) {
-  const SHEETS_CSV = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQTkUQ3UTjLhTTVv29vl_dkOtjweQ41gkUJNgwDkxgb_6Y0OtGX-8FsP7ZeQzNmEykeKifIXI5o42_4/pub?gid=1075508092&single=true&output=csv';
+  const SUPABASE_URL = 'https://cfqrdxiutsaxwupaiogn.supabase.co';
+  const SUPABASE_KEY = 'sb_publishable_FErqHjDSEAz55WOD13Xf6A_CnlMIdAP';
 
   try {
-    const response = await fetch(SHEETS_CSV);
-    const csv = await response.text();
-    const lines = csv.split('\n');
-    const stock = {};
-    for (let i = 1; i < lines.length; i++) {
-      const cols = lines[i].split(',');
-      if (cols.length >= 4) {
-        const ref = cols[0].trim().replace(/^"|"$/g, '');
-        const qty = parseInt(cols[3].trim().replace(/^"|"$/g, ''), 10);
-        if (ref && !isNaN(qty)) stock[ref] = qty;
+    const response = await fetch(`${SUPABASE_URL}/rest/v1/stock?select=ref,qty`, {
+      headers: {
+        'apikey': SUPABASE_KEY,
+        'Authorization': `Bearer ${SUPABASE_KEY}`
       }
-    }
+    });
+    const data = await response.json();
+    const stock = {};
+    data.forEach(item => { stock[item.ref] = item.qty; });
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
