cc5823d74881af07bb2b2e96f6543bbfbc180030
kamilou123456789-beep <kamilou123456789@gmail.com>
2026-07-29 20:20:08 +0200

    Update update-avis-stock.js

--- netlify/functions/update-avis-stock.js
+++ netlify/functions/update-avis-stock.js
@@ -1,18 +1,18 @@
-  exports.handler = async function(event, context) {
-  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby61xxibBlw_VvalSENF96qiCjoda9nOK4ZczqluMcOKsaJq-1EsGa_UIMSzWUZYeSn/exe';
+ exports.handler = async function(event, context) {
+  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby61xxibBlw_VvalSENF96qiCjoda9nOK4ZczqluMcOKsaJq-1EsGa_UIMSzWUZYeSn/exec';
 
   try {
-    const body = event.body;
+    const body = JSON.parse(event.body);
     const response = await fetch(SCRIPT_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: body
+      body: JSON.stringify(body)
     });
-
+    const data = await response.json();
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ success: true })
+      body: JSON.stringify(data)
     };
   } catch (err) {
     return {
